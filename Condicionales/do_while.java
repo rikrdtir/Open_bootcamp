17d82e6e06c94c22361cb5b3d33fd8d2df73fb71
Ricardo tarrillo <71291245+rikrdtir@users.noreply.github.com>
2023-03-07 23:54:31 -0500

    do_while en java
    Ejercicio en condicionales usando do while en java para open bootcamp

---
+++ Condicionales/do_while.java
@@ -0,0 +1,16 @@
+
+/*
+Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+*/
+//by ricardt
+
+public class Main
+{
+    public static void main(String[] args) {
+      int numeroDoWhile = 0;
+      do {
+        numeroDoWhile++;
+        System.out.println(numeroDoWhile);
+      } while (numeroDoWhile < 5);
+    }
+}
